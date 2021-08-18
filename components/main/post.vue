b5f05315ec1e49f4b4a4c849c4a5a76478e42b22
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2021-08-18 22:57:20 +0300

    Little Touch Framework

---
+++ components/main/post.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="post">
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: "post"
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    height: 200px;
+    border: 1px solid black;
+    margin-bottom: 2rem;
+  }
+</style>
